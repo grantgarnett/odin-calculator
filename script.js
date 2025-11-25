47d7919ab3c28ea10179337802731c25ab26d96f
Grant Garnett <grantgarnett01@outlook.com>
2025-11-24 19:57:34 -0600

    Create operate function to call arithmetic

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ let input1 = "";
 let input2 = "";
 let operator = "";
 
-function add(num1, num2) {
+function add (num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1, num2) {
+function subtract (num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1, num2) {
+function multiply (num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2) {
+function divide (num1, num2) {
     if(num2 !== 0) {
         return num1 / num2;
     } else return "ERROR";
 }
 
+operate(operator, num1, num2) {
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "x") {
+        multiply(num1, num2);
+    } else if (operator === "div") {
+        divide(num1, num2);
+    }
+}
