bc378d0cb812a71e4366e73a755d4fc5d1167282
Grant Garnett <grantgarnett01@outlook.com>
2025-11-24 19:53:24 -0600

    Create arithmetic functions and global variables

---
+++ script.js
@@ -0,0 +1,22 @@
+let input1 = "";
+let input2 = "";
+let operator = "";
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(num2 !== 0) {
+        return num1 / num2;
+    } else return "ERROR";
+}
+
