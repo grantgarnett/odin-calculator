96321e3501c14d61860d7ad4a53ddb72d8160d63
Grant Garnett <grantgarnett01@outlook.com>
2025-11-28 14:42:33 -0600

    Create functions to populate the display

--- script.js
+++ script.js
@@ -31,3 +31,27 @@ function operate(operator, num1, num2) {
         divide(num1, num2);
     }
 }
+
+const display = document.querySelector("#display");
+
+const oneButton = document.querySelector("#one-button");
+const twoButton = document.querySelector("#two-button");
+const threeButton = document.querySelector("#three-button");
+const fourButton = document.querySelector("#four-button");
+const fiveButton = document.querySelector("#five-button");
+const sixButton = document.querySelector("#six-button");
+const sevenButton = document.querySelector("#seven-button");
+const eightButton = document.querySelector("#eight-button");
+const nineButton = document.querySelector("#nine-button");
+const zeroButton = document.querySelector("#zero-button");
+
+oneButton.addEventListener("click", () => display.textContent += "1");
+twoButton.addEventListener("click", () => display.textContent += "2");
+threeButton.addEventListener("click", () => display.textContent += "3");
+fourButton.addEventListener("click", () => display.textContent += "4");
+fiveButton.addEventListener("click", () => display.textContent += "5");
+sixButton.addEventListener("click", () => display.textContent += "6");
+sevenButton.addEventListener("click", () => display.textContent += "7");
+eightButton.addEventListener("click", () => display.textContent += "8");
+nineButton.addEventListener("click", () => display.textContent += "9");
+zeroButton.addEventListener("click", () => display.textContent += "0");
